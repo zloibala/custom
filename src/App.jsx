c454abfbfc019f0300ad9aa459b51cc0546a7b01
zloibala <boronova25@gmail.com>
2023-05-16 20:02:04 -0500

    fresh form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignUpForm from "./container/signUpForm/SignUpForm";
+
+function App() {
+  return (
+    <div className="App-container">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/signUpForm/SignUpForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+import "./signUpForm.style.css";
+
+const defaultFormData = {
+  displayName: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState(defaultFormData);
+  const onChangeHandler = (e) => {
+    console.log(e.target.name, ":", e.target.value);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+    const carData = {
+      brand: "",
+      model: "",
+      year: "",
+    };
+  };
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        <h1> Sign Up: </h1>
+        <form onSubmit={onSubmitHandler}>
+          <label htmlFor="display"> Display Name </label>
+          <input
+            name="display"
+            onChange={onChangeHandler}
+            value={formData.displayName}
+          />
+          <label htmlFor="Password"> Password </label>
+          <input
+            name="password"
+            onChange={onChangeHandler}
+            value={formData.password}
+          />
+          <label htmlFor="Confirm Password"> Confirm Password</label>
+          <input
+            name="confirm passowrd"
+            onChange={onChangeHandler}
+            value={formData.confirmPassword}
+          />
+          <br></br>
+          <button type="submit"> Sign Up </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
